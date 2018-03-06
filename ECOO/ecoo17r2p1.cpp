0dc03a1e0226f5aa4297a22adeaa5e2b03e2efd4
YinuoWang <yinuo_wang@yahoo.ca>
2018-03-05 19:55:36 -0500

    Improved ecoo17r2p1 solution

--- ECOO/ecoo17r2p1.cpp
+++ ECOO/ecoo17r2p1.cpp
@@ -7,41 +7,31 @@ using namespace std;
 int testCases = 10;
 pair<string, char>key[4];
 
-char fillThis(pair<string, char>localKey[4], string inputLR) {
+char fillThis(pair<string, char>localKey[4], char left, char right) {
 	for (int i = 0; i<4; ++i) {
-		if (inputLR == localKey[i].first) {
+		if (left == localKey[i].first[0] && right == localKey[i].first[1]) {
 			return localKey[i].second;
 		}
 	}
 }
 
 int main() {
-	while (testCases != 0) {
-		--testCases;
+	while (testCases--){
 		int n, m;
 		cin >> n >> m;
 		for (int i = 0; i<4; ++i) {
 			cin >> key[i].first >> key[i].second;
 		}
-		string currentLine, nextLine = "";
+		string currentLine, nextLine;
 		cin >> currentLine;
 		for (int i = 0; i<m-1; ++i) {
-			string appendThis = "";
-			appendThis += currentLine[n-1];
-			appendThis += currentLine[1];
-			nextLine += fillThis(key, appendThis);
-			for (int k = 1; k<n - 1; ++k) {
-				appendThis = "";
-				appendThis += currentLine[k-1];
-				appendThis += currentLine[k+1];
-				nextLine += fillThis(key, appendThis);
-			}
-			appendThis = "";
-			appendThis += currentLine[n-2];
-			appendThis += currentLine[0];
-			nextLine += fillThis(key, appendThis);
-			currentLine = nextLine;
 			nextLine = "";
+			nextLine += fillThis(key, currentLine[n-1], currentLine[1]);
+			for (int k = 1; k<n - 1; ++k) {
+				nextLine += fillThis(key, currentLine[k-1], currentLine[k+1]);
+			}
+			nextLine += fillThis(key, currentLine[n-2], currentLine[0]);
+			currentLine = nextLine;
 		}
 		cout << currentLine << endl;
 		char asterisk;
