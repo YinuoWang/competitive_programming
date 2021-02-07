97e14a8f730af5e4f4d0860634bb183621e967b9
YinuoWang <yn6wang@edu.uwaterloo.ca>
2021-02-07 13:37:52 -0500

    CodeForces Round 700 D2 - Problems A,B,D

---
+++ CodeForces/r700_d2/r700_d2_A.py
@@ -0,0 +1,78 @@
+def main():
+    cases = int(input())
+    ans_list = []
+    for _ in range(cases):
+        cur_ans = []
+        s = input()
+        for i, char in enumerate(s):
+            if i % 2 == 0:
+                if char == 'a':
+                    cur_ans.append('b')
+                else:
+                    cur_ans.append('a')
+            else:
+                if char == 'z':
+                    cur_ans.append('y')
+                else:
+                    cur_ans.append('z')
+        ans_list.append("".join(cur_ans))
+    
+    print('\n'.join(ans_list))
+
+# region fastio
+import os
+import sys
+from io import BytesIO, IOBase
+BUFSIZE = 8192
+ 
+class FastIO(IOBase):
+    newlines = 0
+ 
+    def __init__(self, file):
+        self._fd = file.fileno()
+        self.buffer = BytesIO()
+        self.writable = "x" in file.mode or "r" not in file.mode
+        self.write = self.buffer.write if self.writable else None
+ 
+    def read(self):
+        while True:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            if not b:
+                break
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines = 0
+        return self.buffer.read()
+ 
+    def readline(self):
+        while self.newlines == 0:
+            b = os.read(self._fd, max(os.fstat(self._fd).st_size, BUFSIZE))
+            self.newlines = b.count(b"\n") + (not b)
+            ptr = self.buffer.tell()
+            self.buffer.seek(0, 2), self.buffer.write(b), self.buffer.seek(ptr)
+        self.newlines -= 1
+        return self.buffer.readline()
+ 
+    def flush(self):
+        if self.writable:
+            os.write(self._fd, self.buffer.getvalue())
+            self.buffer.truncate(0), self.buffer.seek(0)
+ 
+ 
+class IOWrapper(IOBase):
+    def __init__(self, file):
+        self.buffer = FastIO(file)
+        self.flush = self.buffer.flush
+        self.writable = self.buffer.writable
+        self.write = lambda s: self.buffer.write(s.encode("ascii"))
+        self.read = lambda: self.buffer.read().decode("ascii")
+        self.readline = lambda: self.buffer.readline().decode("ascii")
+ 
+ 
+sys.stdin, sys.stdout = IOWrapper(sys.stdin), IOWrapper(sys.stdout)
+input = lambda: sys.stdin.readline().rstrip("\r\n")
+ 
+# endregion
+ 
+if __name__ == "__main__":
+    main()
